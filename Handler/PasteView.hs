3aef5f0cd807ad08227dc9eef5e1dbb4309f34a7
Peter Harpending <peter@harpending.org>
2015-09-11 03:34:00 -0600

    Basic minimal paste functionality!
    Now it's all about adding bells and whistles; the core functionality is there

--- Handler/PasteView.hs
+++ Handler/PasteView.hs
@@ -3,4 +3,13 @@ module Handler.PasteView where
 import Import
 
 getPasteViewR :: PasteId -> Handler Html
-getPasteViewR pasteId = error "Not yet implemented: getPasteViewR"
+getPasteViewR pasteId = do
+  paste <- runDB (get pasteId) >>=
+           \case
+              Just x -> return x
+              Nothing -> notFound
+  defaultLayout $
+    [whamlet|
+      <div .container>
+        #{pasteHtml paste}
+    |]
