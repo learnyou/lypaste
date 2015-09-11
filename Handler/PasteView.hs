9db6307f92486e9d26bb8db76bd4f1e002694a82
Peter Harpending <peter@harpending.org>
2015-09-11 03:13:12 -0600

    Add PasteViewR handler

---
+++ Handler/PasteView.hs
@@ -0,0 +1,6 @@
+module Handler.PasteView where
+
+import Import
+
+getPasteViewR :: PasteId -> Handler Html
+getPasteViewR pasteId = error "Not yet implemented: getPasteViewR"
---
+++ test/Handler/PasteViewSpec.hs
@@ -0,0 +1,10 @@
+module Handler.PasteViewSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getPasteViewR" $ do
+        error "Spec not implemented: getPasteViewR"
+
