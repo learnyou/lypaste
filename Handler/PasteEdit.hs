2a68863dae143ad422fe446bd3006295faaa6cb3
Peter Harpending <peter@harpending.org>
2015-09-11 18:05:01 -0600

    Implement PasteEdit

--- Handler/PasteEdit.hs
+++ Handler/PasteEdit.hs
@@ -1,9 +1,45 @@
 module Handler.PasteEdit where
 
-import Import
+import           Import
+import           Model.Paste
 
 getPasteEditR :: PasteId -> Handler Html
-getPasteEditR pasteId = error "Not yet implemented: getPasteEditR"
+getPasteEditR pasteId = do
+  (widget, enctype) <- generateFormPost (mkFormFor pasteId)
+  defaultLayout $
+    [whamlet|
+      <form role=form method=post target=@{PasteEditR pasteId} enctype=#{enctype}>
+        ^{widget}
+    |]
 
 postPasteEditR :: PasteId -> Handler Html
-postPasteEditR pasteId = error "Not yet implemented: postPasteEditR"
+postPasteEditR pasteId = do
+  ((result, widget), enctype) <- runFormPost (mkFormFor pasteId)
+  case result of
+    FormMissing ->
+      defaultLayout $
+        [whamlet|
+          <p .error>Form is missing
+          <form role=form method=post target=@{PasteEditR pasteId} enctype=#{enctype}>
+            ^{widget}
+        |]
+    FormFailure errors -> do
+      defaultLayout $
+        [whamlet|
+          <ul>
+            $forall err <- errors
+              <li>#{err}
+          <form role=form method=post target=@{PasteEditR pasteId} enctype=#{enctype}>
+            ^{widget}
+        |]
+    FormSuccess paste -> do
+      runDB $ update pasteId
+        [PasteMarkdown =. pasteMarkdown paste
+        ,PasteHtml =. pasteHtml paste
+        ]
+      redirect $ PasteViewR pasteId
+
+mkFormFor :: PasteId -> Html -> MForm Handler (FormResult Paste, Widget)
+mkFormFor pasteId ht = do
+  paste <- lift $ runDB (get404 pasteId)
+  pasteForm (Just (pasteMarkdown paste)) ht
